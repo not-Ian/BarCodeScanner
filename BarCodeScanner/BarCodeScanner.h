64b91c71e5fe9f78e96349044bd0cba07cd8b49c
Ian <ian@pikazoapp.com>
2016-10-14 21:55:05 -0500

    Initial Commit

---
+++ BarCodeScanner/BarCodeScanner.h
@@ -0,0 +1,19 @@
+//
+//  BarCodeScanner.h
+//  BarCodeScanner
+//
+//  Created by Ian Cunningham on 10/14/16.
+//  Copyright © 2016 Ian Cunningham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BarCodeScanner.
+FOUNDATION_EXPORT double BarCodeScannerVersionNumber;
+
+//! Project version string for BarCodeScanner.
+FOUNDATION_EXPORT const unsigned char BarCodeScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BarCodeScanner/PublicHeader.h>
+
+
